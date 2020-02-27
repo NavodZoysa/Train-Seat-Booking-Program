2e67ca7bd0edf1cb3e6ed1fc40b5ff00682085fa
Navod Zoysa <55632254+NavodZoysa@users.noreply.github.com>
2020-02-27 22:12:49 +0530

    first change

---
+++ Train Seating Booking/src/Booking.java
@@ -0,0 +1,122 @@
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Booking extends Application {
+    static final int seatingCapacity = 42;
+    static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Stage stage = new Stage();
+        Pane root = new Pane();
+        root.setStyle("-fx-background-color:GRAY");
+        Scene scene = new Scene(root, 700, 800);
+        consoleMenu(root, stage, scene);
+    }
+
+    List<Integer> bookedList = new ArrayList<>(seatingCapacity);
+    public void createSeats(Pane root,Stage stage, Scene scene, String input) {
+        int colYCord = 60;
+        int labelNo = 0;
+        for (int i = 1; i <= 6; i++) {
+            for (int j = 1; j <= 7; j++) {
+                Label seat = new Label("S-" +(++labelNo));
+                seat.setId(String.valueOf(labelNo));
+                bookedList.add(0);
+                seat.setPrefSize(50, 50);
+                seat.setLayoutX(j * 80);
+                seat.setLayoutY(i * colYCord);
+                root.getChildren().addAll(seat);
+                seat.setStyle("-fx-background-color:GREEN");
+                seat.setAlignment(Pos.CENTER);
+                if(input.equals("a")) {
+                    int selectedSeat = labelNo-1;
+                    seat.setOnMouseClicked(event -> {
+                        if(bookedList.get(selectedSeat)!=1) {
+                            seat.setStyle("-fx-background-color:RED");
+                            bookedList.set(selectedSeat,1);
+                            System.out.println(bookedList);
+                        }
+                    });
+                }
+                else if(input.equals("v")){
+                    System.out.println("v");
+                }
+                else{
+                    System.out.println("e");
+                }
+            }
+        }
+        stage.setTitle("Train Seat Booking Application");
+        stage.setScene(scene);
+        stage.showAndWait();
+        stage.close();
+    }
+
+    public void addCustomerToSeat(Pane root, Stage stage, Scene scene, String input) {
+        createSeats(root, stage, scene, input);
+    }
+
+    public void viewAllSeats(Pane root, Stage stage, Scene scene, String input) {
+        createSeats(root, stage, scene, input);
+    }
+
+    public void displayEmptySeats(Pane root, Stage stage, Scene scene, String input){
+        createSeats(root, stage, scene, input);
+    }
+
+    public void consoleMenu(Pane root, Stage stage, Scene scene) {
+        while(true) {
+            System.out.println("\nWelcome To Fort Railway Station\n" +
+                    "Denuwara Menike Intercity Express Train departure from Colombo to Badulla\n"+
+                    "\nPlease enter 'A' to add a customer to a seat\n" +
+                    "Please enter 'V' to view all seats\n" +
+                    "Please enter 'E' to display empty seats\n" +
+                    "Please enter 'D' to delete customer from seat\n" +
+                    "Please enter 'F' to find the seat for a given customer name\n" +
+                    "Please enter 'S' to store the booking details to a file\n" +
+                    "Please enter 'L' to load the booking details from a file\n" +
+                    "Please enter 'O' to view seats ordered alphabetically by customer name\n" +
+                    "Please enter 'Q' to quit the program");
+            String userInput = scan.next().toLowerCase();
+
+            switch (userInput) {
+                case "a":
+                    addCustomerToSeat(root, stage, scene, userInput);
+                    break;
+                case "v":
+                    viewAllSeats(root, stage, scene, userInput);
+                    break;
+                case "e":
+                    displayEmptySeats(root, stage, scene, userInput);
+                    break;
+                /*case "d":
+                    break;
+                case "f":
+                    break;
+                case "s":
+                    break;
+                case "l":
+                    break;
+                case "o":
+                    break;*/
+                case "q":
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid input! Please enter a valid input ");
+                    break;
+            }
+        }
+    }
+}
